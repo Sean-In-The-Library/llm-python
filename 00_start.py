3cc7ba3375d0f8a10d1cfefdfec80cc8a6deebcb
Sean Harrington <60198848+Sean-In-The-Library@users.noreply.github.com>
2023-06-04 12:40:10 -0700

    The Beginning

--- 00_start.py
+++ 00_start.py
@@ -1,11 +1,18 @@
+import os
 from dotenv import load_dotenv
 from langchain.embeddings import OpenAIEmbeddings
+OPENAI_API_KEY = "os.env('OPENAI_API_KEY')"
+
+# Check if the OPENAI_API_KEY is found
+#if OPENAI_API_KEY is None:
+#    raise ValueError("OPENAI_API_KEY not found in the environment variables. Please check your .env file or system environment variables.")
+
 
 load_dotenv()
 embeddings = OpenAIEmbeddings()
-text = "Algoritma is a data science school based in Indonesia and Supertype is a data science consultancy with a distributed team of data and analytics engineers."
+text = "The Ross-Blakley Law Library is the law library associated with the Sandra Day O'Connor College of Law at Arizona State University."
 doc_embeddings = embeddings.embed_documents([text])
 
-# OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
-# print(OPENAI_API_KEY)
+
+print(OPENAI_API_KEY)
 print(doc_embeddings)
